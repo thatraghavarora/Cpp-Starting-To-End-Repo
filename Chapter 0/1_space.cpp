c322da4509f12d3179d28e665491f27f5995700c
thatraghavarora <raghavarora930@gmail.com>
2025-12-06 16:19:21 +0530

    Chapter

---
+++ Chapter 0/1_space.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Hello";
+    cout<<endl; // Enter In New Line
+    cout<<endl;
+    cout<<"\n";
+    cout<<"World\n";
+
+    cout<<"Hello"<<"_"<<"World";
+}
---
+++ Chapter 0/printingnumber.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<7+7; // The Answer is 14
+    cout<<endl;
+    cout<<"7+7"; // This was Treat as Text Becouse it Was Written inside a string ( " " )
+}
